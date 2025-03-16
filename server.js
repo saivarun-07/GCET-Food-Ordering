77e8c887ff5d7140c987eb01e2bafa56067a1db6
saivarun-07 <bsaivarungupta786@gmail.com>
2025-03-16 20:32:27 +0530

    Update Content Security Policy to allow connections to backend API

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.MONGODB_URI) {
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://*.onrender.com"
+    "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://gcet-food-ordering-backend.onrender.com"
   );
   next();
 });
